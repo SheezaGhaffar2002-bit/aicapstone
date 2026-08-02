846784034b49d7e9f64d26b7fb6697981f23a93c
Sheeza Ghaffar <sheeza.ghaffar202@gmail.com>
2026-08-02 21:13:21 +0500

    Add Expense Tracker project

---
+++ script.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                    },
+                    colors: {
+                        primary: '#6366f1',
+                        secondary: '#8b5cf6',
+                    }
+                }
+            }
+        }
